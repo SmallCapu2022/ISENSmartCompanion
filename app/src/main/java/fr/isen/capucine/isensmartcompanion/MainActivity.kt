09d13a62f90ef3a7b33550cdd355a38df64d133f
Capucine <debailleulcd@gmail.com>
2025-02-24 23:59:49 +0100

    Nav Bar finished !

--- app/src/main/java/fr/isen/capucine/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/capucine/isensmartcompanion/MainActivity.kt
@@ -18,7 +18,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -33,21 +40,53 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import fr.isen.capucine.isensmartcompanion.screens.MainScreen
+import fr.isen.capucine.isensmartcompanion.screens.TabView
 import fr.isen.capucine.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val eventsTab = TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications)
+            val historyTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+
+            // creating a list of all the tabs
+            val tabBarItems = listOf(homeTab, eventsTab, historyTab)
+
+            // creating our navController
+            val navController = rememberNavController()
             ISENSmartCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { TabView(tabBarItems, navController) }) { innerPadding ->
                     Box(Modifier.padding(innerPadding)) {
                         MainScreen(innerPadding)
+                        NavHost(navController = navController, startDestination = homeTab.title) {
+                            composable(homeTab.title) {
+                                Text(homeTab.title)
+                            }
+                            composable(eventsTab.title) {
+                                Text(eventsTab.title)
+                            }
+                            composable(historyTab.title) {
+                                Text(historyTab.title)
+                            }
+                        }
                     }
                 }
             }
@@ -55,54 +94,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MainScreen(innerPaddingValues: PaddingValues) {
-    val context = LocalContext.current
-    var userInput = remember { mutableStateOf("")}
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxSize()
-            .padding(50.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(painterResource(id = R.drawable.isen), context.getString(R.string.isen_logo))
-        Text(context.getString(R.string.app_name))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(16.dp))
-                .background(Color.LightGray)
-                .padding(8.dp)
-        ) {
-            TextField(
-                value = userInput.value,
-                onValueChange = {newValue ->
-                    userInput.value = newValue
-                },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    disabledContainerColor = Color.Transparent
-                ),
-                modifier = Modifier.weight(1F)
-            )
-            OutlinedButton(onClick = {
-                Toast.makeText(context, "user input : ${userInput.value}", Toast.LENGTH_LONG).show()
-            },
-                modifier = Modifier.background(Color.Red, shape = CircleShape),
-                content = {
-                    Image(painterResource(R.drawable.send), "")
-                })
-        }
-
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ISENSmartCompanionTheme {
-        MainScreen(PaddingValues(24.dp))
+        MainScreen(PaddingValues(8.dp))
     }
 }
